e8a763b49d17b65ff667a79cfefdf218b864b316
Vasudev290 <vasukesavulu@gmail.com>
2025-07-05 12:07:20 +0530

    Multiple route

--- app.js
+++ app.js
@@ -2,10 +2,30 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Vasu", lastName: "Dev" });
-});
+app.get(
+  "/user",
+  (req, res, next) => {
+    //res.send("Response!!");
+    next()
+  },
+  (req, res, next) => {
+    //res.send("2nd Response!!");
+    next()
+  },
+  (req, res, next) => {
+    //res.send("3nd Response!!");
+    next()
+  },
+   (req, res, next) => {
+    //res.send("4nd Response!!");
+    next()
+  },
+   (req, res, next) => {
+    //res.send("5nd Response!!");
+    //next()
+  }
+);
+
 //Listen server
 app.listen(6969, () => {
   console.log("Server started and running successfully!");
